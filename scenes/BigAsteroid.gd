82b953927fc8525a8e8f5c46c461cee203db2d60
Diego Solórzano <diegosolor@gmail.com>
2018-01-21 14:53:22 +0100

    potato asteroid!

--- scenes/BigAsteroid.gd
+++ scenes/BigAsteroid.gd
@@ -1,7 +1,9 @@
 extends Area2D
 
 const MAX_SPEED = 400
+const MAX_ANGULAR_SPEED = 2*PI
 var speed
+var angular_speed
 
 func _ready():
 	add_to_group("asteroids")
@@ -9,8 +11,10 @@ func _ready():
 	var angle = rand_range(0, 2*PI)
 	var speed_module = rand_range(0, MAX_SPEED)
 	speed = Vector2(speed_module*cos(angle), speed_module*sin(angle))
+	angular_speed = rand_range(-MAX_ANGULAR_SPEED, MAX_ANGULAR_SPEED)
 	set_process(true)
-	set_scale(Vector2(0.5,0.5))
+	#set_scale(Vector2(0.5,0.5))
 
 func _process(delta):
 	self.set_pos(self.get_pos()+delta*speed)
+	self.set_rot(self.get_rot()+delta*angular_speed)
