5e70dd3e460e24298077303e2f5db55a506722b9
Ricardo Gomez Escalante <ricardogomezescalante@gmail.com>
2018-01-21 23:18:20 +0100

    Increased State singleton functionalities

---
+++ scenes/State.gd
@@ -0,0 +1,16 @@
+extends Node
+
+var score = 0
+var win = false
+var active_scene
+
+func add_score_point():
+	score+=1
+
+func change_scene(scene_path):
+	var scene_instance = load(scene_path).instance()
+	if active_scene != null:
+		active_scene.queue_free()
+	active_scene = scene_instance
+	get_tree().get_root().add_child(scene_instance)
+	
--- scenes/state.gd
+++
@@ -1,7 +0,0 @@
-extends Node
-
-var score = 0
-var win = false
-
-func add_score_point():
-	score+=1
